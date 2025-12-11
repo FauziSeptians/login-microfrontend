0b8be6863c572dc0a5ff221f2dd72d6d23a232c8
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2025-12-12 00:17:23 +0700

    feat: login microfrontend using clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-import ReactQueryProvider from '@/components/providers/ReactQueryProvider';
 import '@/styles/globals.css';
-import { classNames } from '@/utils/classNames';
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
@@ -25,16 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={classNames(
-          `${geistSans.variable}`,
-          `${geistMono.variable}`,
-          'antialiased'
-        )}
-      >
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
