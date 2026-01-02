45881f67640527c7f06ac21480696a990e06b9c6
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2026-01-02 20:45:33 +0700

    fix: routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,28 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { isAuthenticated } = await auth();
+
+  if (isAuthenticated) {
+    return NextResponse.redirect(
+      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL || ''
+    );
+  }
+
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+
+  // If nothing matched, just continue
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
-    '/((?!_next|.*\\.(?:html?|css|js|json|jpg|jpeg|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
 };
